15bedd1e5d649e496e40ed0370d377dc6dfaac46
Anish Asthana <anishasthana1@gmail.com>
2021-02-18 13:35:22 -0500

    Add Dashboard for monitoring Jupyterhub SLIs
    Signed-off-by: Anish Asthana <anishasthana1@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -33,8 +33,8 @@ sed -i "s/<change_route>/$(oc get route prometheus -o jsonpath='{.spec.host}')/g
 sed -i "s/<change_token>/$(oc sa get-token grafana)/g" monitoring/grafana-secrets.yaml
 oc create -n redhat-odh-monitoring -f monitoring/grafana-secrets.yaml || echo "INFO: Grafana secrets already exist."
 
+oc apply -n redhat-odh-monitoring -f monitoring/grafana-dashboards
 oc apply -n redhat-odh-monitoring -f monitoring/grafana.yaml
-
 
 sed -i "s/<prometheus_proxy_secret>/$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)/g" monitoring/prometheus-secrets.yaml
 sed -i "s/<alertmanager_proxy_secret>/$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)/g" monitoring/prometheus-secrets.yaml
