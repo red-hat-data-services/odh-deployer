f067fffea8ddb84e4ad44fd15302ab8a8fd7d0f0
Anish Asthana <anishasthana1@gmail.com>
2021-03-10 12:27:27 -0500

    Move grafana/prometheus objects to specific directories
    Signed-off-by: Anish Asthana <anishasthana1@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -28,27 +28,27 @@ fi
 
 oc new-project $ODH_MONITORING_PROJECT || echo "INFO: $ODH_MONITORING_PROJECT project already exists."
 oc label namespace $ODH_MONITORING_PROJECT openshift.io/cluster-monitoring=true --overwrite=true
-oc apply -n $ODH_MONITORING_PROJECT -f monitoring/cluster-monitor-rbac.yaml
+oc apply -n $ODH_MONITORING_PROJECT -f monitoring/cluster-monitoring/cluster-monitor-rbac.yaml
 
-sed -i "s/<prometheus_proxy_secret>/$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)/g" monitoring/prometheus-secrets.yaml
-sed -i "s/<alertmanager_proxy_secret>/$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)/g" monitoring/prometheus-secrets.yaml
-oc create -n $ODH_MONITORING_PROJECT -f monitoring/prometheus-secrets.yaml || echo "INFO: Prometheus session secrets already exist."
+sed -i "s/<prometheus_proxy_secret>/$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)/g" monitoring/prometheus/prometheus-secrets.yaml
+sed -i "s/<alertmanager_proxy_secret>/$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)/g" monitoring/prometheus/prometheus-secrets.yaml
+oc create -n $ODH_MONITORING_PROJECT -f monitoring/prometheus/prometheus-secrets.yaml || echo "INFO: Prometheus session secrets already exist."
 sleep 5
-sed -i "s/<prom_bearer_token>/$(oc sa -n $ODH_MONITORING_PROJECT get-token prometheus)/g" monitoring/prometheus.yaml
-sed -i "s/<federate_target>/$(oc get -n openshift-monitoring route prometheus-k8s -o jsonpath='{.spec.host}')/g" monitoring/prometheus.yaml
-oc apply -n $ODH_MONITORING_PROJECT -f monitoring/alertmanager-svc.yaml
-sed -i "s/<set_alertmanager_host>/$(oc get route alertmanager -o jsonpath='{.spec.host}')/g" monitoring/prometheus.yaml
+sed -i "s/<prom_bearer_token>/$(oc sa -n $ODH_MONITORING_PROJECT get-token prometheus)/g" monitoring/prometheus/prometheus.yaml
+sed -i "s/<federate_target>/$(oc get -n openshift-monitoring route prometheus-k8s -o jsonpath='{.spec.host}')/g" monitoring/prometheus/prometheus.yaml
+oc apply -n $ODH_MONITORING_PROJECT -f monitoring/prometheus/alertmanager-svc.yaml
+sed -i "s/<set_alertmanager_host>/$(oc get route alertmanager -o jsonpath='{.spec.host}')/g" monitoring/prometheus/prometheus.yaml
 
-oc apply -n $ODH_MONITORING_PROJECT -f monitoring/prometheus.yaml
+oc apply -n $ODH_MONITORING_PROJECT -f monitoring/prometheus/prometheus.yaml
 
-oc apply -f monitoring/grafana-sa.yaml
+oc apply -f monitoring/grafana/grafana-sa.yaml
 
-sed -i "s/<change_proxy_secret>/$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)/g" monitoring/grafana-secrets.yaml
-sed -i "s/<change_route>/$(oc get route prometheus -o jsonpath='{.spec.host}')/g" monitoring/grafana-secrets.yaml
-sed -i "s/<change_token>/$(oc sa get-token grafana)/g" monitoring/grafana-secrets.yaml
-oc create -n $ODH_MONITORING_PROJECT -f monitoring/grafana-secrets.yaml || echo "INFO: Grafana secrets already exist."
+sed -i "s/<change_proxy_secret>/$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)/g" monitoring/grafana/grafana-secrets.yaml
+sed -i "s/<change_route>/$(oc get route prometheus -o jsonpath='{.spec.host}')/g" monitoring/grafana/grafana-secrets.yaml
+sed -i "s/<change_token>/$(oc sa get-token grafana)/g" monitoring/grafana/grafana-secrets.yaml
+oc create -n $ODH_MONITORING_PROJECT -f monitoring/grafana/grafana-secrets.yaml || echo "INFO: Grafana secrets already exist."
 sleep 5
-oc apply -n $ODH_MONITORING_PROJECT -f monitoring/grafana-dashboards
-oc apply -n $ODH_MONITORING_PROJECT -f monitoring/grafana.yaml
+oc apply -n $ODH_MONITORING_PROJECT -f monitoring/grafana/grafana-dashboards
+oc apply -n $ODH_MONITORING_PROJECT -f monitoring/grafana/grafana.yaml
 
-oc apply -n $ODH_MONITORING_PROJECT -f monitoring/rhods-rules.yaml
+oc apply -n $ODH_MONITORING_PROJECT -f monitoring/cluster-monitoring/rhods-rules.yaml
