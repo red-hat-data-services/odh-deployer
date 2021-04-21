e762455fd2982e0a872a6ef877c3aeba726f85b3
Anish Asthana <anishasthana1@gmail.com>
2021-04-21 10:25:15 -0400

    Add error handling message
    Signed-off-by: Anish Asthana <anishasthana1@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -86,11 +86,11 @@ fi
 
 export jupyterhub_prometheus_api_token=$(openssl rand -hex 32)
 sed -i "s/<jupyterhub_prometheus_api_token>/$jupyterhub_prometheus_api_token/g" monitoring/jupyterhub-prometheus-token-secrets.yaml
-oc create -n ${ODH_PROJECT} -f monitoring/jupyterhub-prometheus-token-secrets.yaml
+oc create -n ${ODH_PROJECT} -f monitoring/jupyterhub-prometheus-token-secrets.yaml || echo "INFO: Jupyterhub scrape token already exist."
 
 export jupyterhub_postgresql_password=$(openssl rand -hex 32)
 sed -i "s/<jupyterhub_postgresql_password>/$jupyterhub_postgresql_password/g" jupyterhub/jupyterhub-database-password.yaml
-oc create -n ${ODH_PROJECT} -f jupyterhub/jupyterhub-database-password.yaml
+oc create -n ${ODH_PROJECT} -f jupyterhub/jupyterhub-database-password.yaml || echo "INFO: Jupyterhub Password already exist."
 
 oc apply -n ${ODH_PROJECT} -f opendatahub.yaml
 if [ $? -ne 0 ]; then
