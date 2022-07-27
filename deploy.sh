7d32543402f8f8898d8e81c45896bb740f394689
Anish Asthana <anishasthana1@gmail.com>
2022-07-27 18:09:09 -0400

    Utilize bearer token file instead of manually specifying bearer token in prometheus federation
    Signed-off-by: Anish Asthana <anishasthana1@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -177,8 +177,6 @@ sed -i "s/<prometheus_proxy_secret>/$(openssl rand -hex 32)/g" monitoring/promet
 sed -i "s/<alertmanager_proxy_secret>/$(openssl rand -hex 32)/g" monitoring/prometheus/prometheus-secrets.yaml
 oc create -n $ODH_MONITORING_PROJECT -f monitoring/prometheus/prometheus-secrets.yaml || echo "INFO: Prometheus session secrets already exist."
 
-
-prometheus_token=$(oc::wait::object::availability "oc sa -n $ODH_MONITORING_PROJECT get-token prometheus" 2 30)
 if [ $(oc get clusterversion -o jsonpath='{.items[0].status.desired.version}' | cut -d '.' -f 1) -gt 4 ]; then
     route="prometheus-k8s-federate"
     echo "On OpenShift 5 or higher"
@@ -199,7 +197,6 @@ fi
 ocp_federate_target=$(oc::wait::object::availability "oc get -n openshift-monitoring route $route -o jsonpath='{.spec.host}'" 2 30 | tr -d "'")
 
 sed -i "s/<jupyterhub_prometheus_api_token>/$(oc::wait::object::availability "oc get secret -n $ODH_PROJECT jupyterhub-prometheus-token-secrets -o jsonpath='{.data.PROMETHEUS_API_TOKEN}'" 2 30 | tr -d "'"  | base64 --decode)/g" monitoring/prometheus/prometheus-configs.yaml
-sed -i "s/<prom_bearer_token>/$prometheus_token/g" monitoring/prometheus/prometheus-configs.yaml
 sed -i "s/<federate_target>/$ocp_federate_target/g" monitoring/prometheus/prometheus-configs.yaml
 oc apply -n $ODH_MONITORING_PROJECT -f monitoring/prometheus/alertmanager-svc.yaml
 
